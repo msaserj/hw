9a217722aed7b0c953449dc7fbcd2315f6e50313
msa <serg.ks@gmail.com>
2023-07-20 20:29:46 +0400

    origin2

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import css from './App.module.css'
 import HW5 from "../../../p2-homeworks/h5-rrd-v6/HW5";
 
-
 function App() {
     return (
         <div className={css.App}>
